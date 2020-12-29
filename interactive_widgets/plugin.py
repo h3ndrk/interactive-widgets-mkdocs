2813410b720341e358f2932fb846164f8e644822
Hendrik <git@h3ndrk.de>
2020-12-29 09:56:49 +0100

    Add backend configuration

--- interactive_widgets/plugin.py
+++ interactive_widgets/plugin.py
@@ -21,8 +21,27 @@ log = mkdocs.plugins.log.getChild('interactive-widgets')
 class Plugin(mkdocs.plugins.BasePlugin):
 
     config_scheme = (
+        ('backend_host', mkdocs.config.config_options.Type(str, default='*')),
+        ('backend_port', mkdocs.config.config_options.Type(int, default=80)),
         ('backend_type', mkdocs.config.config_options.Type(str, default='docker')),
+        ('backend_logging_level', mkdocs.config.config_options.Choice(
+            ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+            default='DEBUG',
+        )),
     )
+
+    def on_pre_build(self, *args, **kwargs):
+        self.backend_configuration = {
+            'host': self.config['backend_host'],
+            'port': self.config['backend_port'],
+            'logger_name': 'Server',
+            'logging_level': self.config['backend_logging_level'],
+            'context': {
+                'type': self.config['backend_type'],
+                'logger_name': f'{self.config["backend_type"].capitalize()}Context',
+            },
+            'pages': {},
+        }
 
     def on_post_page(self, output: str, page: mkdocs.structure.pages.Page, *args, **kwargs):
         log.info(
@@ -53,6 +72,15 @@ class Plugin(mkdocs.plugins.BasePlugin):
             ]))
         ]
 
+        page_url = str(pathlib.PurePosixPath('/') / page.url)
+        self.backend_configuration['pages'][page_url] = {
+            'type': self.config['backend_type'],
+            'logger_name_page': 'Page',
+            'logger_name_room_connection': 'RoomConnection',
+            'logger_name_room': f'{self.config["backend_type"].capitalize()}Room',
+            'executors': {},
+        }
+
         for widget in widgets:
             log.info(f'Processing {widget}...')
             widget.tag.replace_with(widget.get_replacement())
@@ -65,6 +93,8 @@ class Plugin(mkdocs.plugins.BasePlugin):
             for tag in widget.get_head_appends():
                 if tag not in soup.head:
                     soup.head.append(tag)
+            self.backend_configuration['pages'][page_url]['executors'][widget.name] = widget.get_backend_configuration(
+            )
 
         # for index, widget_element in enumerate(soup.find_all(['x-button', 'x-image-viewer', 'x-terminal', 'x-text-editor', 'x-text-viewer'])):
         #     if widget_element.name == 'x-button':
@@ -88,3 +118,7 @@ class Plugin(mkdocs.plugins.BasePlugin):
         # print(page_file)
         # print(html, page, args, kwargs)
         return soup.encode_contents(formatter='html5').decode()
+
+    def on_post_build(self, *args, **kwargs):
+        import pprint
+        pprint.pprint(self.backend_configuration)
