159323b2e8a01f8352389f3505c8d72aa2cd9dc9
Hendrik <git@h3ndrk.de>
2021-01-01 11:14:28 +0100

    Fix websocket URL in room connection

--- interactive_widgets/static/RoomConnection.js
+++ interactive_widgets/static/RoomConnection.js
@@ -1,7 +1,8 @@
 class RoomConnection {
   constructor() {
     this.subscribers = {};
-    this.webSocket = new WebSocket(this.getWebSocketUrl);
+    this.ensureLocationHash();
+    this.webSocket = new WebSocket(this.getWebSocketUrl());
     this.webSocket.addEventListener("message", this.handleMessage.bind(this));
     this.webSocket.addEventListener("close", this.handleClose.bind(this));
     this.webSocket.addEventListener("error", this.handleError.bind(this));
@@ -23,14 +24,26 @@ class RoomConnection {
   ensureLocationHash() {
     // https://gist.github.com/johnelliott/cf77003f72f889abbc3f32785fa3df8d
     if (!location.hash.match(new RegExp(/^#[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i))) {
-      location.hash = `#${uuidv4()}`;
+      location.hash = `#${this.uuidv4()}`;
     }
   }
   getUuidFromLocationHash() {
     return location.hash.substr(1);
   }
   getWebSocketUrl() {
-    return `${location.protocol === "https:" ? "wss:" : "ws:"}//${location.host}${location.pathname}/ws?roomName=${this.getUuidFromLocationHash()}`;
+    const webSocketUrl = new URL(window.location);
+    if (webSocketUrl.protocol === "https:") {
+      webSocketUrl.protocol = "wss:";
+    } else {
+      webSocketUrl.protocol = "ws:";
+    }
+    webSocketUrl.hash = "";
+    const hasTrailingSlash = webSocketUrl.pathname[webSocketUrl.pathname.length - 1] === "/";
+    webSocketUrl.pathname = `${webSocketUrl.pathname}${hasTrailingSlash ? "" : "/"}ws`;
+    const webSocketUrlParams = new URLSearchParams(webSocketUrl.search);
+    webSocketUrlParams.append("roomName", this.getUuidFromLocationHash());
+    webSocketUrl.search = webSocketUrlParams;
+    return webSocketUrl.toString();
   }
   handleMessage(event) {
     const message = JSON.parse(event.data);
