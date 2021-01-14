b69794f1c66b11880e18d0758d90dcb9ea99ebd6
Hendrik <git@h3ndrk.de>
2021-01-14 21:03:21 +0100

    Improve text editor widget

--- plugin/static/TextEditorWidget.js
+++ plugin/static/TextEditorWidget.js
@@ -10,14 +10,7 @@ class TextEditorWidget {
     this.element.classList = ["interactive-widgets-text-editor"];
     this.contentsElement = document.createElement("div");
     this.contentsElement.classList.add("contents");
-    this.setupError(btoa("There is no data"));
-    this.element.appendChild(this.contentsElement);
-    this.captionElement = document.createElement("div");
-    this.captionElement.classList.add("caption");
-    this.captionElement.innerText = `Editing text of ${this.file}`;
-    this.element.appendChild(this.captionElement);
-  }
-  setupButtons(hasError) {
+
     const buttonsElement = document.createElement("div");
     buttonsElement.classList.add("buttons");
     const buttonCreateElement = document.createElement("button");
@@ -27,33 +20,29 @@ class TextEditorWidget {
       // this.sendMessage(null);
     });
     buttonsElement.appendChild(buttonCreateElement);
-    if (!hasError) {
-      const buttonSaveElement = document.createElement("button");
-      buttonSaveElement.innerText = "Save";
-      buttonSaveElement.addEventListener("click", () => {
-        // TODO
-        // this.sendMessage(null);
-      });
-      buttonsElement.appendChild(buttonSaveElement);
-      const buttonDeleteElement = document.createElement("button");
-      buttonDeleteElement.innerText = "Delete";
-      buttonDeleteElement.addEventListener("click", () => {
-        // TODO
-        // this.sendMessage(null);
-      });
-      buttonsElement.appendChild(buttonDeleteElement);
-    }
+    const spacerBetweenCreateAndSaveElement = document.createElement("div");
+    spacerBetweenCreateAndSaveElement.classList.add("spacer", "hide-on-error");
+    buttonsElement.appendChild(spacerBetweenCreateAndSaveElement);
+    const buttonSaveElement = document.createElement("button");
+    buttonSaveElement.classList.add("hide-on-error");
+    buttonSaveElement.innerText = "Save";
+    buttonSaveElement.addEventListener("click", () => {
+      // TODO
+      // this.sendMessage(null);
+    });
+    buttonsElement.appendChild(buttonSaveElement);
+    const spacerBetweenSaveAndDeleteElement = document.createElement("div");
+    spacerBetweenSaveAndDeleteElement.classList.add("spacer", "hide-on-error");
+    buttonsElement.appendChild(spacerBetweenSaveAndDeleteElement);
+    const buttonDeleteElement = document.createElement("button");
+    buttonDeleteElement.classList.add("hide-on-error");
+    buttonDeleteElement.innerText = "Delete";
+    buttonDeleteElement.addEventListener("click", () => {
+      // TODO
+      // this.sendMessage(null);
+    });
+    buttonsElement.appendChild(buttonDeleteElement);
     this.contentsElement.appendChild(buttonsElement);
-  }
-  setupError(error) {
-    while (this.contentsElement.firstChild) {
-      this.contentsElement.removeChild(this.contentsElement.firstChild);
-    }
-
-    this.contentsElement.classList.remove("with-contents");
-    this.contentsElement.classList.add("with-error");
-
-    this.setupButtons(true);
 
     const errorContainerElement = document.createElement("div");
     errorContainerElement.classList.add("error-container");
@@ -67,30 +56,41 @@ class TextEditorWidget {
     titleElement.classList.add("title");
     titleElement.innerText = `Cannot view ${this.file}`;
     errorElement.appendChild(titleElement);
-    const descriptionElement = document.createElement("div");
-    descriptionElement.classList.add("description");
-    descriptionElement.innerText = atob(error);
-    errorElement.appendChild(descriptionElement);
+    this.errorDescriptionElement = document.createElement("div");
+    this.errorDescriptionElement.classList.add("description");
+    errorElement.appendChild(this.errorDescriptionElement);
     errorContainerElement.appendChild(errorElement);
     this.contentsElement.appendChild(errorContainerElement);
-  }
-  setupContents(contents) {
-    while (this.contentsElement.firstChild) {
-      this.contentsElement.removeChild(this.contentsElement.firstChild);
-    }
-
-    this.contentsElement.classList.remove("with-error");
-    this.contentsElement.classList.add("with-contents");
-
-    this.setupButtons(false);
 
     const editorElement = document.createElement("div");
     editorElement.classList.add("editor");
     this.editor = CodeMirror(editorElement, {
       lineNumbers: true,
-      value: atob(contents),
     });
     this.contentsElement.appendChild(editorElement);
+    this.editor.refresh();
+
+    this.setupError(btoa("There is no data"));
+
+    this.element.appendChild(this.contentsElement);
+
+    this.captionElement = document.createElement("div");
+    this.captionElement.classList.add("caption");
+    this.captionElement.innerText = `Editing text of ${this.file}`;
+    this.element.appendChild(this.captionElement);
+  }
+  setupError(error) {
+    this.contentsElement.classList.remove("with-contents");
+    this.contentsElement.classList.add("with-error");
+
+    this.errorDescriptionElement.innerText = atob(error);
+  }
+  setupContents(contents) {
+    this.contentsElement.classList.remove("with-error");
+    this.contentsElement.classList.add("with-contents");
+
+    this.editor.setValue(atob(contents));
+    this.editor.refresh();
   }
   handleMessage(message) {
     if (message.type != "output") {
