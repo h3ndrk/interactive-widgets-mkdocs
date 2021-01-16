2786fe784db1ff22729974e286035392b41e74d8
Hendrik <git@h3ndrk.de>
2021-01-16 19:52:46 +0100

    Only create configuration files if dynamic pages exist

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -52,8 +52,6 @@ class Plugin(mkdocs.plugins.BasePlugin):
             'pages': {},
         }
         self.static_files = set()
-        # with (pathlib.Path(__file__).parent/'static/test.txt').open()as f:
-        #     print(f.read())
 
     def on_post_page(self, output: str, page: mkdocs.structure.pages.Page, *args, **kwargs):
         log.info(
@@ -115,80 +113,77 @@ class Plugin(mkdocs.plugins.BasePlugin):
 
             return soup.encode_contents(formatter='html5').decode()
 
-        # print(page.__dict__)
-        # page_file: mkdocs.structure.files.File = page.file
-        # print(page_file)
-        # print(html, page, args, kwargs)
         log.info(f'No widgets in {page}, building as static page')
         return output
 
     def on_post_build(self, config: mkdocs.config.base.Config, *args, **kwargs):
-        log.info('Writing interactive-widgets-backend.json...')
-        with (config['site_dir_parent'] / 'interactive-widgets-backend.json').open('w') as f:
-            json.dump(self.backend_configuration, f, indent=2)
+        if len(self.backend_configuration['pages']) > 0:
+            log.info('Writing interactive-widgets-backend.json...')
+            with (config['site_dir_parent'] / 'interactive-widgets-backend.json').open('w') as f:
+                json.dump(self.backend_configuration, f, indent=2)
 
-        log.info('Copying static files...')
-        for static_file in self.static_files:
-            source_static_file = pathlib.Path(
-                __file__).parent / 'static' / pathlib.Path(static_file)
-            target_static_file = config['site_dir'] / pathlib.Path(static_file)
-            target_static_file.parent.mkdir(parents=True, exist_ok=True)
-            if source_static_file.is_file():
-                shutil.copy(
-                    source_static_file,
-                    target_static_file,
-                )
-            else:
-                shutil.copytree(
-                    source_static_file,
-                    target_static_file,
-                    dirs_exist_ok=True,
-                )
+            log.info('Copying static files...')
+            for static_file in self.static_files:
+                source_static_file = pathlib.Path(
+                    __file__).parent / 'static' / pathlib.Path(static_file)
+                target_static_file = config['site_dir'] / pathlib.Path(static_file)
+                target_static_file.parent.mkdir(parents=True, exist_ok=True)
+                if source_static_file.is_file():
+                    shutil.copy(
+                        source_static_file,
+                        target_static_file,
+                    )
+                else:
+                    shutil.copytree(
+                        source_static_file,
+                        target_static_file,
+                        dirs_exist_ok=True,
+                    )
 
-        # TODO: corner case: only static files
-        log.info('Writing interactive-widgets-nginx.conf...')
-        with (config['site_dir_parent'] / 'interactive-widgets-nginx.conf').open('w') as f:
-            print('upstream backend {', file=f)
-            print('    server interactive-widgets-backend;', file=f)
-            print('}', file=f)
-            print('server {', file=f)
-            print('    listen       80;', file=f)
-            print('    server_name  localhost;', file=f)
-            print('    location / {', file=f)
-            print('        root /usr/share/nginx/html;', file=f)
-            print('        index index.html index.htm;', file=f)
-            print('    }', file=f)
-            for page_url in self.backend_configuration['pages'].keys():
-                websocket_url = pathlib.PurePosixPath(page_url) / 'ws'
-                proxy_url = f'http://backend{websocket_url}'
-                print(f'    location = {websocket_url} {{', file=f)
-                print(f'        proxy_pass {proxy_url};', file=f)
-                print('        proxy_http_version 1.1;', file=f)
-                print('        proxy_set_header Upgrade $http_upgrade;', file=f)
-                print('        proxy_set_header Connection "Upgrade";', file=f)
-                print('        proxy_set_header Host $host;', file=f)
+            # TODO: corner case: only static files
+            log.info('Writing interactive-widgets-nginx.conf...')
+            with (config['site_dir_parent'] / 'interactive-widgets-nginx.conf').open('w') as f:
+                print('upstream backend {', file=f)
+                print('    server interactive-widgets-backend;', file=f)
+                print('}', file=f)
+                print('server {', file=f)
+                print('    listen       80;', file=f)
+                print('    server_name  localhost;', file=f)
+                print('    location / {', file=f)
+                print('        root /usr/share/nginx/html;', file=f)
+                print('        index index.html index.htm;', file=f)
                 print('    }', file=f)
-            print('}', file=f)
+                for page_url in self.backend_configuration['pages'].keys():
+                    websocket_url = pathlib.PurePosixPath(page_url) / 'ws'
+                    proxy_url = f'http://backend{websocket_url}'
+                    print(f'    location = {websocket_url} {{', file=f)
+                    print(f'        proxy_pass {proxy_url};', file=f)
+                    print('        proxy_http_version 1.1;', file=f)
+                    print('        proxy_set_header Upgrade $http_upgrade;', file=f)
+                    print('        proxy_set_header Connection "Upgrade";', file=f)
+                    print('        proxy_set_header Host $host;', file=f)
+                    print('    }', file=f)
+                print('}', file=f)
 
-        log.info('Writing Dockerfile...')
-        with (config['site_dir_parent'] / 'Dockerfile').open('w') as f:
-            print('FROM nginx', file=f)
-            print('RUN rm /etc/nginx/conf.d/default.conf /usr/share/nginx/html/*', file=f)
-            print('COPY interactive-widgets-nginx.conf /etc/nginx/conf.d/', file=f)
-            print('COPY static/ /usr/share/nginx/html/', file=f)
+            log.info('Writing Dockerfile...')
+            with (config['site_dir_parent'] / 'Dockerfile').open('w') as f:
+                print('FROM nginx', file=f)
+                print('RUN rm /etc/nginx/conf.d/default.conf /usr/share/nginx/html/*', file=f)
+                print('COPY interactive-widgets-nginx.conf /etc/nginx/conf.d/', file=f)
+                print('COPY static/ /usr/share/nginx/html/', file=f)
 
-        log.info('Writing docker-compose.yaml...')
-        with (config['site_dir_parent'] / 'docker-compose.yaml').open('w') as f:
-            print('version: "3"', file=f)
-            print('services:', file=f)
-            print('  interactive-widgets-nginx:', file=f)
-            print('    image: interactive-widgets-nginx', file=f)
-            print('    build: .', file=f)
-            print('    ports:', file=f)
-            print('    - "80:80"', file=f)
-            print('  interactive-widgets-backend:', file=f)
-            print('    image: interactive-widgets-backend', file=f)
-            print('    volumes:', file=f)
-            print('      - "./interactive-widgets-backend.json:/usr/src/app/interactive-widgets-backend.json"', file=f)
-            print('      - "/var/run/docker.sock:/var/run/docker.sock"', file=f)
-            print('    command: ["interactive-widgets-backend", "interactive-widgets-backend.json"]', file=f)
+            log.info('Writing docker-compose.yaml...')
+            with (config['site_dir_parent'] / 'docker-compose.yaml').open('w') as f:
+                print('version: "3"', file=f)
+                print('services:', file=f)
+                print('  interactive-widgets-nginx:', file=f)
+                print('    image: interactive-widgets-nginx', file=f)
+                print('    build: .', file=f)
+                print('    ports:', file=f)
+                print('    - "80:80"', file=f)
+                print('  interactive-widgets-backend:', file=f)
+                print('    image: interactive-widgets-backend', file=f)
+                print('    volumes:', file=f)
+                print('      - "./interactive-widgets-backend.json:/usr/src/app/interactive-widgets-backend.json"', file=f)
+                print('      - "/var/run/docker.sock:/var/run/docker.sock"', file=f)
+                print('    command: ["interactive-widgets-backend", "interactive-widgets-backend.json"]', file=f)
