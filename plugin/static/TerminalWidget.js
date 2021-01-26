28284ae1641e09a719b98a1598372b5f429a2e7b
Hendrik <git@h3ndrk.de>
2021-01-26 11:47:42 +0100

    Change quotes

--- plugin/static/TerminalWidget.js
+++ plugin/static/TerminalWidget.js
@@ -21,29 +21,29 @@ class TerminalWidget {
   }
   setupTerminal() {
     this.terminal = new Terminal({
-      fontFamily: 'JetBrains Mono',
+      fontFamily: "JetBrains Mono",
       theme: {
-        foreground: '#000',
-        background: '#eee',
-        black: '#000',
-        blue: '#2196f3',
-        brightBlack: '#000',
-        brightBlue: '#2196f3',
-        brightCyan: '#00acc1',
-        brightGreen: '#43a047',
-        brightMagenta: '#9c27b0',
-        brightRed: '#f44336',
-        brightWhite: '#000',
-        brightYellow: '#fdd835',
-        cursor: '#000',
-        cursorAccent: '#000',
-        cyan: '#00acc1',
-        green: '#43a047',
-        magenta: '#9c27b0',
-        red: '#f44336',
-        selection: 'rgba(0, 0, 0, 0.25)',
-        white: '#000',
-        yellow: '#fdd835',
+        foreground: "#000",
+        background: "#eee",
+        black: "#000",
+        blue: "#2196f3",
+        brightBlack: "#000",
+        brightBlue: "#2196f3",
+        brightCyan: "#00acc1",
+        brightGreen: "#43a047",
+        brightMagenta: "#9c27b0",
+        brightRed: "#f44336",
+        brightWhite: "#000",
+        brightYellow: "#fdd835",
+        cursor: "#000",
+        cursorAccent: "#000",
+        cyan: "#00acc1",
+        green: "#43a047",
+        magenta: "#9c27b0",
+        red: "#f44336",
+        selection: "rgba(0, 0, 0, 0.25)",
+        white: "#000",
+        yellow: "#fdd835",
       },
     });
     this.fitAddon = new FitAddon.FitAddon();
@@ -68,7 +68,7 @@ class TerminalWidget {
       }
     });
     this.fitAddon.fit();
-    window.addEventListener('resize', () => {
+    window.addEventListener("resize", () => {
       this.fitAddon.fit();
     });
   }
